f5e64cf25e12985a681ed65ba5868d94eaf1fd49
Xavier <xrabert@gmail.com>
2025-08-13 16:02:02 +0200

    gsap: first animation

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ gsap.from(".menu-item", {
 const text = SplitType.create(".hero-title", { types: "words, chars" });
 
 text.chars.map((char, index) => {
+  let charOriginalColor = window.getComputedStyle(char).color;
   let charsTimeLine = gsap.timeline();
 
   charsTimeLine.from(char, {
@@ -33,11 +34,47 @@ text.chars.map((char, index) => {
     ease: "back.out",
   });
 
-  charsTimeLine.from(char, {
-    color: `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`,
-  });
+  charsTimeLine.from(
+    char,
+    {
+      color: getRandomColor(),
+      duration: 1,
+    },
+    "-=.25"
+  );
+
+  char.addEventListener("mouseenter", charsHover);
+
+  function charsHover() {
+    gsap
+      .timeline()
+      .to(char, {
+        y: gsap.utils.random(-50, 50),
+        x: gsap.utils.random(-50, 50),
+        rotate: gsap.utils.random(-90, 90),
+        scale: gsap.utils.random(0.5, 1.5),
+        color: getRandomColor(),
+        duration: 0.5,
+        ease: "back.out",
+        onComplete: () => char.removeEventListener("mouseenter", charsHover),
+      })
+      .to(char, {
+        y: 0,
+        x: 0,
+        rotate: 0,
+        scale: 1,
+        color: charOriginalColor,
+        delay: 0.5,
+        onComplete: () =>
+          setTimeout(() => {
+            char.addEventListener("mouseenter", charsHover);
+          }, 100),
+      });
+  }
 });
 
 function getRandomColor() {
-  return gsap.utils.random(0, 255);
+  const getColor = () => gsap.utils.random(0, 255);
+
+  return `rgb(${getColor()},${getColor()},${getColor()})`;
 }
